69fe028a2161dc27f8c75071dd55e31d1fe3f463
shiftj18 <stjkl@foxmail.com>
2022-08-08 16:46:48 +0800

    feat: use esbuild for minify by --minify-esb

--- packages/jsx2mp-loader/src/utils/minifyCode.js
+++ packages/jsx2mp-loader/src/utils/minifyCode.js
@@ -1,12 +1,21 @@
 const terser = require('terser');
 const csso = require('csso');
 const prettyData = require('pretty-data').pd;
+const esbuild = require('esbuild');
+
+const useEsBuildMinify = process.argv.indexOf('--minify-esb') > -1;
 
 function minifyJS(source) {
+  if (useEsBuildMinify) {
+    return esbuild.transformSync(source, { minify: true, target: 'es6' }).code;
+  }
   return terser.minify(source).code;
 }
 
 function minifyCSS(source) {
+  if (useEsBuildMinify) {
+    return esbuild.transformSync(source, { loader: 'css', minify: true }).code;
+  }
   return csso.minify(source, {
     restructure: false
   }).css;
